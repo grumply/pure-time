b0cdda627825c9fd1cb0bef2db720ae38cc03318
S. M. Hickman <sean@grump.ly>
2018-05-14 19:13:14 -0500

    Add default.nix

---
+++ default.nix
@@ -0,0 +1,10 @@
+{ mkDerivation, base, hashable, pure-json, pure-txt, stdenv, time
+}:
+mkDerivation {
+  pname = "pure-time";
+  version = "0.7.0.0";
+  src = ./.;
+  libraryHaskellDepends = [ base hashable pure-json pure-txt time ];
+  homepage = "github.com/grumply/pure-time";
+  license = stdenv.lib.licenses.bsd3;
+}
